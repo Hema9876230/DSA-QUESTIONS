e445818fb59e86414609a4bf9aebdfa9f51a8cdc
Hema Rani <ranihema513@gmail.com>
2026-07-06 09:47:27 +0530

    Clarify purpose of variable 'k' in removeElement
    Added a comment to clarify the purpose of variable 'k'.

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-
+//K store element that is not equal to val 
         int k = 0;
 
         for(int i = 0; i < nums.length; i++) {
